74da724b3194188f4a6f5a1042b7e3aff202e246
Monkey-cee <ezekielbakwena1@gmail.com>
2025-04-16 22:41:42 +0200

    Added files via upload
    We collaborated on whatsapp to create the two classes ( Team.java,Table.java), we discussed the attributes needed for both classes and added getters and setters

---
+++ Table.java
@@ -0,0 +1,52 @@
+//Table.java
+
+public class Table {
+    private matchesPlayed;
+    private int won;
+    private int drawn;
+    private int lost;
+    private int points;
+
+    // Constructor for the Table class
+    public Table(int matchesPlayed, int won, int drawn, int lost, int points) {
+        this.matchesPlayed = matchesPlayed;
+        this.won = won;
+        this.drawn = drawn;
+        this.lost = lost;
+        this.points = points;
+    }
+
+        //Basic  Getters and Setters
+        public int getMatchesPlayed() {
+            return matchesPlayed;
+        }
+        public void setMatchesPlayed(int matchesPlayed) {
+            this.matchesPlayed = matchesPlayed;
+        }
+        public int getWon() {
+            return won;
+        }
+        public void setWon(int won) {
+            this.won = won;
+        }
+        public int getDrawn() {
+            return drawn;
+        }
+        public void setDrawn(int drawn) {
+            this.drawn = drawn;
+        }
+        public int getLost() {
+            return lost;
+        }
+        public void setLost(int lost) {
+            this.lost = lost;
+        }
+        public int getPoints() {
+            return points;
+        }
+        public void setPoints(int points) {
+            this.points = points;
+        }
+
+
+}
---
+++ Team.java
@@ -0,0 +1,67 @@
+//Team.java
+
+public class Team {
+    private String teamName;
+    private String Stadium;
+    private int founded;
+    private String homeTown;
+    private String rivals;
+    private Table table; // Table being the other class in our package
+
+        // Constructor for the Team class
+        public Team(String teamName, String Stadium, int founded, String homeTown, String rivals) {
+            this.teamName = teamName;
+            this.Stadium = Stadium;
+            this.founded = founded;
+            this.homeTown = homeTown;
+            this.rivals = rivals;
+            this.table = table;
+        }
+            //Getters and Setters for the Team class
+            public String getTeamName() {
+                return teamName;
+            }
+              public void setTeamName(String teanName){
+                this.teamName = teamName;
+              }
+
+                public String getStadium() {
+                    return Stadium;
+                }
+                public void setStadium(String Stadium) {
+                    this.Stadium = Stadium;
+                }
+                  public int getFounded() {
+                    return founded;
+                     }
+
+                public void setFounded(int founded) {
+                    this.founded = founded;
+                }
+
+                public String getHomeTown() {
+                    return homeTown;
+                }
+                
+                public void setHomeTown(String homeTown) {
+                    this.homeTown = homeTown;
+                }
+
+                public String getRivals() {
+                    return rivals;
+                }
+
+                public void setRivals(String rivals) {
+                    this.rivals = rivals;
+                }
+
+                public Table getTable() {
+                    return table;
+                }
+                public void setTable(Table table) {
+                    this.table = table;
+                }
+   }   
+
+
+                
