8f0ff13fbf0fddd0ff4f441469493e17aa5a6a0b
Ezekiel Bakwena <ezekielbakwena1@gmail.com>
2025-04-21 07:50:21 +0000

    Added a description of our parent class

---
+++ ClubEntity.java
@@ -0,0 +1,31 @@
+/**************************************************************************
+ *Program name: ClubEntity.java
+ * @author: Gugu Ketshabe           ID: 202407232 
+ * @author: Lone Mosime             ID: 202401044
+ * @author: Devin Phokoletso        ID: 202402388
+ * @author: Lekgotla Maphorisa      ID: 202400965
+ * @author: Ezekiel Bakwena         ID: 202406103
+ * @version:1.00
+ * Description: A simple parent class to Team, Table and Matchday classes to demonstrate inheritance which
+ *              contains the club name and year founded.
+ * How to Compile: javac ClubEntity.java
+ * How to run: java  ClubEntity
+ ********************************************************************************/
+//Parent class
+
+public class ClubEntity{
+    protected String clubName;
+    protected int yearFounded;
+
+    public ClubEntity(String clubName, int yearFounded){
+        this.clubName = clubName;
+        this.yearFounded = yearFounded;
+    }
+
+        public void displayinfo(){
+            System.out.println("Entity name: "+clubName);
+            System.out.println("Year founded: "+yearFounded);
+
+        }
+
+}
--- README.md
+++ README.md
@@ -1,23 +1,28 @@
 # Botswana-Premier-League
  A Botswana based digital platform for storing the statistical points of a club in a season.
 
+ Our parent class is ClubEntity
+
  Our two domain classes so far appropriately established are Team and Table.
 
-1st Defining the attributes on each class
+
+
+** Defining the attributes on each class
+ Parent class ClubEntity has a protected attribute named:
+ clubName;
+
 
 Team class private attributes include:
-  Team name
   Stadium
-  Founded
   Home Town
   rivals
   table
 
 Table class private attributes include:
- Matches played
+  Matches played
   Wins
   Losses
   Draws 
   Points
 
-2nd Adding constructors to both our domain classes
+** Adding constructors to both our domain classes
