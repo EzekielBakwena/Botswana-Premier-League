8b968dc4a5ce2c7c9d51cf9a74b9fbadec6ed0b7
LoneMosime77 <lonerulez@gmail.com>
2025-04-21 09:12:30 +0000

    Creating a tester class to test objects

---
+++ Tester.java
@@ -0,0 +1,19 @@
+/**************************************************************************
+ *Program name: Tester.java
+ * @author: Gugu Ketshabe           ID: 202407232 
+ * @author: Lone Mosime             ID: 202401044
+ * @author: Devin Phokoletso        ID: 202402388
+ * @author: Lekgotla Maphorisa      ID: 202400965
+ * @author: Ezekiel Bakwena         ID: 202406103
+ * @version:1.00
+ * Description: A main class thats used to test our classes 
+ * How to compile: javac Tester.java
+ * How to run: java  Tester
+ ********************************************************************************/
+//Tester.java
+
+public class Tester{
+    public static void main (String[] args){
+        
+    }
+}
