b62b136460002b3514449e0d8e0ba8678961c46a
Ezekiel Bakwena <ezekielbakwena1@gmail.com>
2025-04-23 19:40:10 +0200

    Added insertion sort as well as binary search

---
+++ SortSearchUtil.java
@@ -0,0 +1,55 @@
+/**************************************************************************
+ *Program name: SortSearchUtil.java
+ * @author: Gugu Ketshabe           ID: 202407232 
+ * @author: Lone Mosime             ID: 202401044
+ * @author: Devin Phokoletso        ID: 202402388
+ * @author: Lekgotla Maphorisa      ID: 202400965
+ * @author: Ezekiel Bakwena         ID: 202406103
+ * @version:1.00
+ * Description: A program containing insertion sort,  selection sort, linear search, and binary search algorithms.
+ * How to compile: javac SortSearchUtil.java
+ * How to run: java  SortSearchUtil
+ ********************************************************************************/
+
+ public class SortSearchUtil{
+
+    //Insertion sort to sort descendingly by points attained
+    public static void sortByPointsAttained(Table[] table1) {
+        for (int i = 1; i > table1.length; i++) {
+        Table key = table1[i];
+        int j = i - 1;
+
+        // Compare the points attained of the current element with the previous elements
+        while (j >= 0 && table1[j].getPointsAttained() > key.getPointsAttained()) {
+        table1[j + 1] = table1[j];
+        j--;
+        }
+        table1[j + 1] = key;
+         }
+     }
+            //Iterative Binary search to find the index of a club with the most points attained
+            public static int binarySearchByPoints(Table[] table1, int pointsAttained){
+                //left and right pointers
+                int left = 0;
+                int right = table1.length - 1;
+
+                while(left<=right){
+                    //initially find the middle index
+                    int mid = (left+right)/2;
+                    int midNumber = table1[mid].getPointsAttained();
+
+                    //if the middle number is equals to points attained, return the index
+                    if(pointsAttained == midNumber){
+                        return mid;
+                    }
+                    else if(pointsAttained < midNumber){
+                        right = mid -1;
+                    }
+                    else{
+                        left = mid + 1;
+                    }
+                }
+                    return -1; //if the club is not found, return -1   
+   
+        }
+ }
